5403f381cd79bb99f66e4bc442283f03d85eb292
Dhanashri Parkar <dhanashri@live.in>
2025-02-19 11:42:03 -0500

    Added numpy exercises file

---
+++ numpy_exercise.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a1 = np.array([1,2,3])
+print(a1)
+
+#2D array
+a2 = np.array([[1,2,3],[4,5,6]])
+print(a2)
+
+#arrange, linspace
+a3 = np.arange(3,10,2)
+print(a3)
+
+a4 = np.linspace(2,10,3)
+print(a4)
